58a573e0f33d98da573806fb2e803309ff533f93
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-08-28 18:47:52 -0400

    Setup urls.py for portfolio app

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from portfolio import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^rango/', include('portfolio.urls')),
     url(r'^admin/', admin.site.urls),
 ]
