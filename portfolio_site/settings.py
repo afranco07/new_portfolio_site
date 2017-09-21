d2e510ac9e0297c1fd1b5a402b0502b1ddfcbaec
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-09-21 19:29:36 -0400

    Changed some db settings

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,6 +78,8 @@ WSGI_APPLICATION = 'portfolio_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+db_from_env = dj_database_url.config(conn_max_age=500)
+
 
 DATABASES = {
     'default': {
@@ -88,6 +91,7 @@ DATABASES = {
         'PORT': '',
     }
 }
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -127,5 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = [STATIC_DIR,]
