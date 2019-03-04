8ff133c33020caeb839f41ad5c3e1344752f7d96
Alberto Franco <alberto.franco86@qmail.cuny.edu>
2019-03-04 13:01:35 -0500

    Upgraded to django 2 and added golang section

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,16 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+
 class Language(models.Model): 
     language = models.CharField(max_length=128, unique=True)
 
     def __str__(self):
         return self.language
 
+
 class Project(models.Model):
     title = models.CharField(max_length=128, blank=False)
     modal_title = models.CharField(max_length=128, blank=False)
-    language = models.ForeignKey(Language)
+    language = models.ForeignKey(Language, on_delete=True)
     img_source = models.URLField(blank=False)
     info = models.TextField(blank=False)
     gif = models.URLField(blank=False)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from portfolio.models import Project, Language
+from portfolio.models import Project
 import random
 
 # Create your views here.
+
 
 def index(request):
     all_projects = Project.objects.all()
@@ -16,18 +16,22 @@ def index(request):
 
     return render(request, 'portfolio/infoTabs.html', {"randomProjects": random_projects})
 
+
 def projects(request):
     ios_projects = Project.objects.filter(language__language='iOS')
     java_projects = Project.objects.filter(language__language='Java')
     html_projects = Project.objects.filter(language__language='HTML/CSS')
     python_projects = Project.objects.filter(language__language='Python')
+    golang_projects = Project.objects.filter(language__language='Golang')
     projects = {
         "iOSProjects": ios_projects,
         "JavaProjects": java_projects,
         "HTML": html_projects,
         "PythonProjects": python_projects,
+        "GolangProjects": golang_projects
     }
     return render(request, 'portfolio/projectsPage.html', projects)
+
 
 def about(request):
     return render(request, 'portfolio/aboutPage.html', {})
